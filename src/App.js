15ca80d51da7eacf1adfd96bb0a20cb34010ff1e
최유솔 <115677084+choi-yo-sol@users.noreply.github.com>
2022-10-18 16:09:58 +0900

    Revert "branch test"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Hello World!!!!!!
-        brach test -lee
       </header>
     </div>
   );
